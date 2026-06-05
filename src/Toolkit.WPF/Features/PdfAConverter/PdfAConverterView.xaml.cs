de07117ccc5b02e56bc2d21712de4e23a25488a6
chez1s <71917288+TRIBUI106@users.noreply.github.com>
2026-06-05 16:40:44 +0700

    feat(pdfa): add PdfAConverterView XAML and code-behind

---
+++ src/Toolkit.WPF/Features/PdfAConverter/PdfAConverterView.xaml.cs
@@ -0,0 +1,9 @@
+namespace Toolkit.WPF.Features.PdfAConverter;
+
+public partial class PdfAConverterView : System.Windows.Controls.UserControl
+{
+    public PdfAConverterView()
+    {
+        InitializeComponent();
+    }
+}
